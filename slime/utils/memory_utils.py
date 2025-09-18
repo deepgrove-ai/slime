93f9f0e54af442a070e31d3346d30173a6b24f89
Jeffrey Li <jeffrey.dot.li@gmail.com>
2025-09-18 20:00:31 +0000

    fix: memory utils logs

--- slime/utils/memory_utils.py
+++ slime/utils/memory_utils.py
@@ -2,6 +2,7 @@ import gc
 import torch
 import torch.distributed as dist
 from .logging import configure_logging
+import time
 
 logger = configure_logging(__name__)
 
@@ -20,10 +21,10 @@ def available_memory():
     torch.cuda.reset_peak_memory_stats(device)
     return {
         "gpu": str(torch.cuda.current_device()),
-        "curr_alloc_gb": 1e-9 * mem["allocated_bytes.all.current"],
-        "peak_alloc_gb": 1e-9 * mem["allocated_bytes.all.peak"],
-        "curr_resv_gb": 1e-9 * mem["reserved_bytes.all.current"],
-        "peak_resv_gb": 1e-9 * mem["reserved_bytes.all.peak"],
+        # "curr_alloc_gb": 1e-9 * mem["allocated_bytes.all.current"],
+        # "peak_alloc_gb": 1e-9 * mem["allocated_bytes.all.peak"],
+        # "curr_resv_gb": 1e-9 * mem["reserved_bytes.all.current"],
+        # "peak_resv_gb": 1e-9 * mem["reserved_bytes.all.peak"],
         "total_GB": round(total / (1024**3), 2),
         "free_GB": round(free / (1024**3), 2),
         "used_GB": round((total - free) / (1024**3), 2),
@@ -32,4 +33,6 @@ def available_memory():
 
 def print_memory(msg):
     # if dist.get_rank() == 0:
-    logger.info(f"Memory-Usage {msg} {dist.get_rank()}:", available_memory())
+    # Print utc time
+    print(f"time: {time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())} Memory-Usage {msg} {dist.get_rank()}: {available_memory()}")
+    # logger.info(f"Memory-Usage {msg} {dist.get_rank()}:", available_memory())
