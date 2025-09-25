aee46f374bda352e3ee3ad5006ee246b19c433f6
Jeffrey Li <jeffrey.dot.li@gmail.com>
2025-09-25 21:21:42 +0000

    fix: ray get offload

--- train.py
+++ train.py
@@ -59,13 +59,13 @@ def train(args):
         ray.get(actor_model.async_connect(critic_model))
 
     if args.offload:
-        ray.get(ray.get(rollout_manager.onload.remote(tags=[GPU_MEMORY_TYPE_WEIGHTS])))
+        (ray.get(rollout_manager.onload.remote(tags=[GPU_MEMORY_TYPE_WEIGHTS])))
 
     # always update weight first so that sglang has the loaded weights from training.
     ray.get(actor_model.async_update_weights())
 
     if args.offload:
-        ray.get(ray.get(rollout_manager.onload.remote(tags=[GPU_MEMORY_TYPE_KV_CACHE])))
+        (ray.get(rollout_manager.onload.remote(tags=[GPU_MEMORY_TYPE_KV_CACHE])))
 
     # train loop.
     # note that for async training, one can change the position of the sync operation(ray.get).
@@ -78,8 +78,8 @@ def train(args):
         rollout_data_ref = ray.get(rollout_manager.generate.remote(rollout_id))
 
         if args.offload:
-            offload = ray.get(ray.get(rollout_manager.offload.remote()))
-            logger.info(f"Offloaded {offload}")
+            offload_ref = ray.get(rollout_manager.offload.remote())
+            logger.info(f"{offload_ref=}")
 
         if args.use_critic:
             critic_train_handle = critic_model.async_train(rollout_id, rollout_data_ref)
@@ -105,12 +105,12 @@ def train(args):
             else:
                 ray.get(actor_model.async_offload())
 
-            ray.get(ray.get(rollout_manager.onload.remote(tags=[GPU_MEMORY_TYPE_WEIGHTS])))
+            (ray.get(rollout_manager.onload.remote(tags=[GPU_MEMORY_TYPE_WEIGHTS])))
 
         ray.get(actor_model.async_update_weights())
 
         if args.offload:
-            ray.get(ray.get(rollout_manager.onload.remote(tags=[GPU_MEMORY_TYPE_KV_CACHE])))
+            (ray.get(rollout_manager.onload.remote(tags=[GPU_MEMORY_TYPE_KV_CACHE])))
 
         if args.eval_interval is not None and (
             (rollout_id + 1) % args.eval_interval == 0
